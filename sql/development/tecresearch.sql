6748af3163b2278738e7d72047cc0936d26076fb
tecresearch <bnlv1212@gmail.com>
2025-03-16 16:41:15 +0530

    Html completed

--- sql/development/tecresearch.sql
+++ sql/development/tecresearch.sql
@@ -1,2 +1,19 @@
 CREATE DATABASE tecresearch;
 SHOW DATABASES;
+USE tecresearch;
+CREATE TABLE users(
+id int ,
+name varchar(100),
+role varchar(10)
+);
+show tables;
+desc users;
+select * from users;
+
+truncate table users;
+create table users(
+id int auto_increment primary key,
+name varchar(100),
+role varchar(10)
+);
+
